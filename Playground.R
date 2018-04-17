aeed476064e0abe9ce8441f8b77bd0ab1704f892
davestroud <david@greymatter-analytics.com>
2018-04-17 14:52:27 -0500

    Working version of code

--- Playground.R
+++ Playground.R
@@ -1,18 +1,57 @@
 # Load the following packages
+library(keras)
+library(lime)
+library(tidyquant)
+library(rsample)
+library(recipes)
+library(yardstick)
+library(corrr)
+library(readxl)
 library(tidyverse)
-library(caret)
+library(glmnet)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
 
-DDSA <- read_excel("CaseStudy2-data.xlsx")
-
-nrow(DDSA)
-
-table(DDSA$Attrition)
-
-prop.table(table(DDSA$Attrition))
-
-summary(DDSA$MonthlyIncome)
-
-summary(DDSA$TotalWorkingYears)
+raw_DDSA <- read_excel("CaseStudy2-data.xlsx")
 
 
-table(DDSA$JobRole)
+DDSA <- raw_DDSA%>%
+  mutate_if(is.character, as.factor) %>%
+  select(Attrition, everything())
+
+
+glimpse(DDSA)
+
+
+set.seed(777)
+library(caTools)
+
+split = sample.split(DDSA$Attrition, SplitRatio = 0.70)
+
+# Create training and testing sets
+train = subset(DDSA, split == TRUE)
+test = subset(DDSA, split == FALSE)
+
+
+#Load Random Forest package
+library(randomForest)
+
+
+modelRf = randomForest(Attrition ~ ., data=train, ntree = 100, mtry = 5, importance = TRUE, method="class")
+
+print(modelRf)
+
+#OOB vs No. Of Trees
+# Out-of-bag (OOB) error ~ a method of measuring the prediction error of random forests, 
+# boosted decision trees, and other machine learning models utilizing bootstrap aggregating 
+#(bagging) to sub-sample data samples used for training.
+plot(modelRf, main="")
+legend("topright", c("OOB", "0", "1"), text.col=1:6, lty=1:3, col=1:3)
+title(main="Error Rates Random Forest")
+
+
+## List the importance of the variables.
+impVar <- round(randomForest::importance(modelRf), 2)
+impVar[order(impVar[,3], decreasing=TRUE),]
+
