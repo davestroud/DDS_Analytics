9f3fb5f463ee3e6d6c653315a7a82c1ef654a653
davestroud <david@greymatter-analytics.com>
2018-04-30 16:54:54 -0500

    add code snippits

--- Wine.R
+++ Wine.R
@@ -2,14 +2,36 @@ library(tidyverse)
 library(caret)
 library(readr)
 
-winequality_red <- read_csv("winequality-red.csv")
-View(winequality_red)
+overfit <- read_csv("overfit.csv")
 
-# Create custom trainControl: myControl
 myControl <- trainControl(
   method = "cv", number = 10,
   summaryFunction = twoClassSummary,
-  classProbs = TRUE, # IMPORTANT!
+  classProbs = TRUE, # Super important!
   verboseIter = TRUE
 )
 
+set.seed(42)
+  model <- train(y ~ ., overfit, method = "glmnet",
+                 trControl = myControl)
+# Plot results
+  plot(model)
+  
+  expand.grid(alpha = 0:1,
+              lambda = seq(0.0001, 1, length = 100))
+  
+  # Train glmnet with custom trainControl and tuning: model
+  model <- train(
+    y ~ ., overfit,
+    tuneGrid = expand.grid(alpha = 0:1,
+                           lambda = seq(0.0001, 1, length = 20)),
+    method = "glmnet",
+    trControl = myControl
+  )
+  
+  # Print model to console
+  model
+  
+  # Print maximum ROC statistic
+  max(model[["results"]][["ROC"]])
+  
