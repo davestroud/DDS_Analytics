fc8df3ae2df36b2853d6a56110a45cfe6315a22f
davestroud <david@greymatter-analytics.com>
2018-04-30 20:31:39 -0500

    Add knn imputation model

--- WisconsinBC.R
+++ WisconsinBC.R
@@ -4,4 +4,20 @@ library(tidyverse)
 library(readr)
 
 data <- read_csv("data.csv")
-View(data)
+
+# Apply median imputation: model
+model <- train(
+  x = breast_cancer_x, y = breast_cancer_y,
+  method = "glm",
+  trControl = myControl,
+  preProcess = "medianImpute"
+)
+
+# Apply KNN imputation: model2
+model2 <- train(
+  x = breast_cancer_x, y = breast_cancer_y,
+  method = "glm",
+  trControl = myControl,
+  preProcess = "knnImpute"
+)
+
